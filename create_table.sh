1296e39c86a4b690fd50ff709d81423c00f99c6d
georgezhyuan <yuangeorge9981@gmail.com>
2017-10-03 14:17:47 -0400

    modified for loop to output individual patient ids

--- create_table.sh
+++ create_table.sh
@@ -5,8 +5,9 @@
 patient_ids = $1
 for patient_id in "${patient_ids[@]}"
 do
-	local patient = $(getPatient $patient_id)
-	addPatient $patient
+#	local patient = $(getPatient $patient_id)
+#	addPatient $patient
+	echo $patient_id
 done
 
 function addPatient {
