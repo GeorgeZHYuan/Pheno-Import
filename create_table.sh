5a6d5242a58739d969001498451d8783154fceb7
georgezhyuan <yuangeorge9981@gmail.com>
2017-10-04 19:32:01 -0400

    commented and refined code

--- create_table.sh
+++ create_table.sh
@@ -7,17 +7,20 @@ patient_dataset=()
 patients_found=()
 patients_ommited=()
 
-for patient_id in "${patient_ids[@]}"; do
-	temp=$(curlToPheno "patients" $patient_id)
-	if ! [[ "$temp" =~ ^-?[0-9]+$ ]] ; then
-   		patient_dataset+=($temp)
+# Finds and stores patient JSON data array to patient_dataset
+for patient_id in "${patient_ids[@]}"; do			
+	data=$(curlToPheno "patients" $patient_id)		
+	if ! [[ "$data" =~ ^-?[0-9]+$ ]] ; then			# add to patient data array if valid data
+   		patient_dataset+=($data)
 		patients_found+=($patient_id)
-	else
+	else											# add to ommited array if invalid data
 		patients_ommited+=($patient_id)
 	fi	
 done
 
-amount_found=${#patients_found[@]}
+# Stats for Patient data 
+amount_found=${#patients_found[@]}					
 amount_not_found=${#patients_ommited[@]}
 
+# Log results (loggit.sh)
 patientsLog $amount_found $amount_not_found "${patients_found[@]}" "${patients_ommited[@]}"
