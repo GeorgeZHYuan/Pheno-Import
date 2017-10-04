2dd871176011930bc01de2e61e21a638edad1066
georgezhyuan <yuangeorge9981@gmail.com>
2017-10-04 12:50:45 -0400

    curlToPheno returns different things based on status code of request

--- get_data.sh
+++ get_data.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
 
 function getPatient {
-	url="$HOST:$PORT/rest/patients/$1"
-	local patient = $(curlPheno $url)
+	patient_id=$1
+	url=$HOST:$PORT/rest/families/$patient_id
+
+	local patient = $(curlToPheno url="$HOST:$PORT/rest/patients/$patient_id")
 	echo $patient
 }
 
 function getFamily {
-	url="$HOST:$PORT/rest/patients/$1"
-	local family = $(curlPheno $url)
+	family_id=$1
+	url=$HOST:$PORT/rest/families/$family_id
+
+	local family = $(curlToPheno $url)
 	echo $family
 }
 
 function curlToPheno {
-	request="curl -u $USER:$PSWD -X GET $1"
+	url=$1
+	args="-u $USER:$PSWD -X GET $url"
 
-	response=$($request)
-#	echo $response
+	status=$(curl -o -i -s -w "%{http_code}\n" $args)
+	if [[ $status -eq 200 ]]; then
+		response=$($request)
+		echo $response
+	else
+		response=$status
+		echo $status
+	fi;
 
-	status=$($request -I)
-	echo $status
-#	loggit "$request" "$response"
+	loggit "$request" "$response"
 }
 
 function loggit {
+	echo "Logging curl results to" $LOGFILE
     echo $(date '+%d/%m/%Y %H:%M:%S') >> $LOGFILE
     echo "Command:" $1 >> $LOGFILE
     echo $2 $'\r' $'\r' >> $LOGFILE
 }
-
