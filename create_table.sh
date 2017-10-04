2833229fca2cbfaa738ef7b94bd4d5953c8c81d1
georgezhyuan <yuangeorge9981@gmail.com>
2017-10-04 15:56:25 -0400

    patients found and not found will be added into found/ommited arrays

--- create_table.sh
+++ create_table.sh
@@ -3,20 +3,32 @@
 . ./get_data.sh 
 
 patient_ids=("$@")
-i=0
+patient_dataset=()
+patient_added=()
+patient_ommited=()
 for patient_id in "${patient_ids[@]}"
 do
-	temp=$(curlToPheno "patients" $patient_id $'\r')
+	temp=$(curlToPheno "patients" $patient_id)
 	if ! [[ "$temp" =~ ^-?[0-9]+$ ]] ; then
-   		patient_dataset[$i]=$temp
-		i+=1
+   		patient_dataset+=($temp)
+		patient_added+=($patient_id)
+	else
+		patient_ommited+=($patient_id)
 	fi	
 done
 
-for data in "${patient_dataset[@]}"
-do
-	echo $data
-done
+#echo "Patients found"
+#for id in "${patient_added[@]}"
+#do
+#	echo $id
+#done
+#echo
+
+#echo "Patients not found"
+#for id in "${patient_ommited[@]}"
+#do
+#	echo $id
+#done
 
 
 
