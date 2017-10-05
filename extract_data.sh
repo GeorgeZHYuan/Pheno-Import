b7970ee3fb3b74be7151945a741c19cd96b850aa
rimpeyd <radiazco@uwaterloo.ca>
2017-10-05 16:05:25 -0600

    optimized extracting data

--- extract_data.sh
+++ extract_data.sh
@@ -1,45 +1,47 @@
 #!/bin/bash
 
-#Assume data file has all required column labels, data will be append to it
+#Assume data file has all required column labels, data will be appended to it
 
 #Software jq needs to be downloaded
 #sudo apt-get install jq
 
+declare -a arr=(".report_id" ".external_id" ".patient_name.first_name" ".patient_name.last_name" ".sex" ".life_status" ".date_of_birth.year" ".date_of_birth.month" ".date_of_birth.day" ".date_of_death.year" ".date_of_death.month" ".date_of_death.day")
+
 data_JSON=("$@")
 
-#printf "\n" >> extracted_data.txt
-
 patient_data=()
-#echo -e "$a\t" >> extracted_data.txt
 
-#a=$(( $a + 1 ))
+a=1
 
 #Do a for loop to add the data by iteration
-#patient_data+=$(jq -r '.report_id' ${data_JSON[$i]})
 
-patient_data+=$(jq -r '.report_id' sample_data.JSON)
+patient_data+=$a
 patient_data+="	"
-patient_data+=$(jq -r '.external_id' sample_data.JSON)
+patient_data+=$(jq -r ${arr[0]} ${data_JSON[$i]})
 patient_data+="	"
-patient_data+=$(jq -r '.patient_name .first_name' sample_data.JSON)
+patient_data+=$(jq -r ${arr[1]} ${data_JSON[$i]})
 patient_data+="	"
-patient_data+=$(jq -r '.patient_name .last_name' sample_data.JSON)
+patient_data+=$(jq -r ${arr[2]} ${data_JSON[$i]})
 patient_data+="	"
-patient_data+=$(jq -r '.sex' sample_data.JSON)
+patient_data+=$(jq -r ${arr[3]} ${data_JSON[$i]})
 patient_data+="	"
-patient_data+=$(jq -r '.life_status' sample_data.JSON)
+patient_data+=$(jq -r ${arr[4]} ${data_JSON[$i]})
 patient_data+="	"
-patient_data+=$(jq -r '.date_of_birth .year' sample_data.JSON)
+patient_data+=$(jq -r ${arr[5]} ${data_JSON[$i]})
 patient_data+="	"
-patient_data+=$(jq -r '.date_of_birth .month' sample_data.JSON)
+patient_data+=$(jq -r ${arr[6]} ${data_JSON[$i]})
 patient_data+="	"
-patient_data+=$(jq -r '.date_of_birth .day' sample_data.JSON)
+patient_data+=$(jq -r ${arr[7]} ${data_JSON[$i]})
 patient_data+="	"
-patient_data+=$(jq -r '.death_date .day' sample_data.JSON)
+patient_data+=$(jq -r ${arr[8]} ${data_JSON[$i]})
+patient_data+="	"
+patient_data+=$(jq -r ${arr[9]} ${data_JSON[$i]})
+patient_data+="	"
+patient_data+=$(jq -r ${arr[10]} ${data_JSON[$i]})
+patient_data+="	"
+patient_data+=$(jq -r ${arr[11]} ${data_JSON[$i]})
 patient_data+="	"
 
-#jq -r '.patient_name .last_name'  sample_data.JSON >> extracted_data.txt
+a=$(( $a + 1 ))
 
 echo "${patient_data[@]}" >> extracted_data.txt
-
-#echo -e "\t" >> extracted_data.txt
