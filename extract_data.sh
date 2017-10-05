d6ac457eebcc08c2227613944ab713829c37c57d
rimpeyd <radiazco@uwaterloo.ca>
2017-10-05 11:54:55 -0600

    extracts basic info about patient

--- extract_data.sh
+++ extract_data.sh
@@ -5,22 +5,41 @@
 #Software jq needs to be downloaded
 #sudo apt-get install jq
 
-a=1
+data_JSON=("$@")
 
 #printf "\n" >> extracted_data.txt
 
-echo -e "$a\t" >> extracted_data.txt
+patient_data=()
+#echo -e "$a\t" >> extracted_data.txt
 
-a=$(( $a + 1 ))
+#a=$(( $a + 1 ))
 
-jq -r '.patient_name .first_name'  sample_data.JSON >> extracted_data.txt 
-jq -r '.patient_name .last_name'  sample_data.JSON >> extracted_data.txt 
+#Do a for loop to add the data by iteration
+#patient_data+=$(jq -r '.report_id' ${data_JSON[$i]})
+
+patient_data+=$(jq -r '.report_id' sample_data.JSON)
+patient_data+="	"
+patient_data+=$(jq -r '.external_id' sample_data.JSON)
+patient_data+="	"
+patient_data+=$(jq -r '.patient_name .first_name' sample_data.JSON)
+patient_data+="	"
+patient_data+=$(jq -r '.patient_name .last_name' sample_data.JSON)
+patient_data+="	"
+patient_data+=$(jq -r '.sex' sample_data.JSON)
+patient_data+="	"
+patient_data+=$(jq -r '.life_status' sample_data.JSON)
+patient_data+="	"
+patient_data+=$(jq -r '.date_of_birth .year' sample_data.JSON)
+patient_data+="	"
+patient_data+=$(jq -r '.date_of_birth .month' sample_data.JSON)
+patient_data+="	"
+patient_data+=$(jq -r '.date_of_birth .day' sample_data.JSON)
+patient_data+="	"
+patient_data+=$(jq -r '.death_date .day' sample_data.JSON)
+patient_data+="	"
+
+#jq -r '.patient_name .last_name'  sample_data.JSON >> extracted_data.txt
+
+echo "${patient_data[@]}" >> extracted_data.txt
 
 echo -e "\t" >> extracted_data.txt
-
-sed ':a;N;$!ba;s/\n/ /g' extracted_data.txt
-
-
-
-
-
