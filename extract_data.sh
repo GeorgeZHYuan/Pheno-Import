c70b7c2d9f86f725cb58bac22a1404c98ea7ce1f
georgezhyuan <yuangeorge9981@gmail.com>
2017-10-05 19:49:53 -0400

    adjusted jq command to work on variables

--- extract_data.sh
+++ extract_data.sh
@@ -7,25 +7,22 @@
 
 declare -a arr=(".report_id" ".external_id" ".patient_name.first_name" ".patient_name.last_name" ".sex" ".life_status" ".date_of_birth.year" ".date_of_birth.month" ".date_of_birth.day" ".date_of_death.year" ".date_of_death.month" ".date_of_death.day")
 
+subjID=1
 data_JSON=("$@")
 
 rm extracted_data.txt
 cp new_study_template.txt extracted_data.txt
 
-a=1
-#data=0
-
 #Do a for loop to add the data by iteration
 for data in "${data_JSON[@]}"; do
 	patient_data=()
-	patient_data+=$a
-	patient_data+="	"
+	patient_data+=$subjID"	"
 	for label in "${arr[@]}"; do
-		patient_data+=$(jq -r $label $data)
-		patient_data+="	"
+		res=$(echo $data | jq  -r $label )
+		if ! [[ "$res" == null ]]; then
+			patient_data+=$res"	"
+		fi;
 	done
 	echo "${patient_data[@]}" >> extracted_data.txt
-	a=$(( $a + 1 ))
+	subjID=$(( $subjID + 1 ))
 done
-
-
