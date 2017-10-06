aa77b4c8556ed4a1e65f4907cc50fa2d606b55f5
georgezhyuan <yuangeorge9981@gmail.com>
2017-10-06 16:44:33 -0400

    pheno-import.sh automatically pulls patiend ids from patient_ids.txt

--- pheno_import.sh
+++ pheno_import.sh
@@ -3,14 +3,13 @@
 source conf/vars
 source ./loggit.sh
 
-filename=$1
 patients=()
 
 # Aquires a list of patient IDs from "filename"
 # This is the list of patients that the user wants to import
 while IFS='' read -r line || [[ -n "$line" ]]; do	# Iterates through "filename" line by line until eof
     patients+=($line)								# Adds each line as new element to patients array
-done < "$filename"
+done < "$PATIENT_ID_LIST"
 
 # Creates patient data tables according to patients ID in the patients array
 ./create_table.sh "${patients[@]}"
