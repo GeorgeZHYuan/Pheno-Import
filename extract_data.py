22afafc3dc1dd31f283c0d0498ab45ad3fd6f003
Rimpeyd <rimpeyd@gmail.com>
2017-10-11 11:25:44 -0600

    Updated python code

--- extract_data.py
+++ extract_data.py
@@ -2,14 +2,33 @@
 
 import json
 
-patientData = open('sample_data.txt','r')
 extractedData = open('extracted_data.txt', 'a')
-#text=patientData.read()
-#print(text)
-
-wjson = patientData.read()
+wjson = open('sample_data.txt','r').read()
 wjdata = json.loads(wjson)
+subjID = 1
 
-#print wjdata['report_id']
+dataCategories = ["report_id", "external_id", "patient_name"]
 
-extractedData.write(wjdata['report_id'])
+extractedData.write('\n')
+
+#Make for-loop for elements in the array
+i = 0
+extractedData.write("{}".format(subjID))
+
+#while i < len(dataCategories):
+#	if dataCategories[i] in wjson:
+#		extractedData.write('\t' + wjdata[dataCategories[i]])
+#		i+=1
+#	else:
+#		i+=1
+
+while i < len(dataCategories):
+	try:
+		extractedData.write('\t' + wjdata[dataCategories[i]])
+		i+=1
+	except:
+		i+=1
+
+subjID+=1
+
+extractedData.close()
