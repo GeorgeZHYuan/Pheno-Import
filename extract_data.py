5e0fd19458e8822a21fc02180e5147f1c4cffc24
Rimpeyd <rimpeyd@gmail.com>
2017-10-13 14:53:12 -0600

    Added more features to python script

--- extract_data.py
+++ extract_data.py
@@ -2,7 +2,8 @@
 
 import json
 
-extractedData = open('extracted_data.txt', 'a')
+extractedData = open('extracted_data.txt', 'a+')
+templateFile = open('data_template.txt', 'a')
 wjson = open('sample_data.txt','r').read()
 wjdata = json.loads(wjson)
 subjID = 1
@@ -17,30 +18,67 @@ extractedData.write('\n')
 
 i=0
 j=0
+arrayPosition = []
+arrayLength = []
+
 extractedData.write('{}'.format(subjID))
 
 while i < len(dataCategories):
+        if(isinstance(dataCategories[i], list)):
+                arrayPosition.append(i)
+                arrayLength.append(len(dataCategories[i]))
+        i+=1
+
+i=0
+while i < len(dataCategories):
 
 	if(isinstance(dataCategories[i], list)):
-		print isinstance(dataCategories[i], list)
-		#length=len(wjdata.get('ethnicity').get('maternal_ethnicity'))
 		j=0
-		while j < len(wjdata.get('ethnicity').get('maternal_ethnicity')):
-			temp = dataCategories[i][j]
-			extractedData.write('\t' + '{}'.format(temp))
-			j+=1
+		#while j < len(dataCategories[i]):
+		temp = dataCategories[i][j]
+                extractedData.write('\t' + '{}'.format(temp))
+		templateFile.write('\t' + '{}'.format(temp))
+		j+=1
 		i+=1
 	else:
 		temp = dataCategories[i]
 		extractedData.write('\t' + '{}'.format(temp))
+		templateFile.write('\t' + '{}'.format(temp))
 		i+=1
+
+#Pseduo-code
+#For the length of the arrayPostion or arrayLength (should be the same)
+#If length of array is 1+
+#print new line, ID in first line, a bunch of tabs which reflect the arrayPostion, and print second array elements
+#Repeat until no more elements in the array.
+
+i=0
+j=0
+k=1
+if(len(arrayPosition) > 1):        
+        while i < len(arrayPosition):
+                extractedData.write('\n' + '{}'.format(subjID))
+                while j < (arrayPosition[i]):
+                        extractedData.write('\t')
+			j+=1
+			if j > (arrayPosition[i]-1):
+				extractedData.write('\t' + '{}'.format(dataCategories[arrayPosition[i]][k]))
+		print i
+                i+=1
+
 	
 subjID+=1
 
-#print isinstance(wjdata.get('ethnicity').get('maternal_ethnicity'), list)
-#print len(wjdata.get('ethnicity').get('maternal_ethnicity'))
-#print wjdata.get('ethnicity').get('maternal_ethnicity')[0]
+print arrayPosition
+print arrayLength
+print dataCategories[arrayPosition[0]][0]
+#print i
+
+#print extractedData.tell()
+#extractedData.seek(-1,2)
+#extractedData.read()
+#extractedData.write("Testing...")
 
 extractedData.close()
 
-#Add data in array to templates file
+#If array has multiple elements, make a new line with 
