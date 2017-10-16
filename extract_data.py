bf1615cc2230a67f1f1aa6fbc92b9f375db98a06
Rimpeyd <rimpeyd@gmail.com>
2017-10-16 14:31:06 -0600

    Updated python file

--- extract_data.py
+++ extract_data.py
@@ -16,69 +16,28 @@ dataCategories = [wjdata.get("report_id"), wjdata.get("external_id"), wjdata.get
 
 extractedData.write('\n')
 
-i=0
-j=0
-arrayPosition = []
-arrayLength = []
 
-extractedData.write('{}'.format(subjID))
 
-while i < len(dataCategories):
-        if(isinstance(dataCategories[i], list)):
-                arrayPosition.append(i)
-                arrayLength.append(len(dataCategories[i]))
-        i+=1
-
-i=0
-while i < len(dataCategories):
-
-	if(isinstance(dataCategories[i], list)):
-		j=0
-		#while j < len(dataCategories[i]):
-		temp = dataCategories[i][j]
-                extractedData.write('\t' + '{}'.format(temp))
-		templateFile.write('\t' + '{}'.format(temp))
-		j+=1
-		i+=1
-	else:
-		temp = dataCategories[i]
-		extractedData.write('\t' + '{}'.format(temp))
-		templateFile.write('\t' + '{}'.format(temp))
-		i+=1
-
-#Pseduo-code
-#For the length of the arrayPostion or arrayLength (should be the same)
-#If length of array is 1+
-#print new line, ID in first line, a bunch of tabs which reflect the arrayPostion, and print second array elements
-#Repeat until no more elements in the array.
-
-i=0
-j=0
-k=1
-if(len(arrayPosition) > 1):        
-        while i < len(arrayPosition):
-                extractedData.write('\n' + '{}'.format(subjID))
-                while j < (arrayPosition[i]):
-                        extractedData.write('\t')
-			j+=1
-			if j > (arrayPosition[i]-1):
-				extractedData.write('\t' + '{}'.format(dataCategories[arrayPosition[i]][k]))
-		print i
-                i+=1
-
-	
+with open('tmp/test.tmp', 'a') as file:
+	max_array_size = 1
+	for label in dataCategories:
+		if (isinstance(label, list)):
+			max_array_size = max(max_array_size, len(label))
+			file.write(str(label[0])+'\t')
+		else:
+			file.write(str(label)+'\t')
+	file.write('\n')
+	for j in range(1, max_array_size):
+		file.write(str(dataCategories[0])+'\t')
+		for label2 in dataCategories[1:]:
+			if (isinstance(label2, list)):
+				if len(label2) > j:
+					file.write(str(label2[j])+'\t')
+			else:
+				#file.write(str(label2)+'\t')
+				file.write('\t')
+		file.write('\n')
+			
 subjID+=1
 
-print arrayPosition
-print arrayLength
-print dataCategories[arrayPosition[0]][0]
-#print i
-
-#print extractedData.tell()
-#extractedData.seek(-1,2)
-#extractedData.read()
-#extractedData.write("Testing...")
-
 extractedData.close()
-
-#If array has multiple elements, make a new line with 
