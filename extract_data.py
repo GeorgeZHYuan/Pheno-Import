9c1af8b897ae0c0f662f2ed56be3e0cb7f1d9c8d
Rimpeyd <rimpeyd@gmail.com>
2017-10-19 11:32:31 -0600

    Python now clears previous data

--- extract_data.py
+++ extract_data.py
@@ -3,10 +3,19 @@
 import json
 import sys
 
-extractedData = open('import-data/Clinical-data/PHENOTIPS_clinical.txt', 'a+')
-#templateFile = open('data_template.txt', 'a')
+extractedData = open('import-data/Clinical-data/PHENOTIPS_clinical.txt', 'w')
+templateFile = open('templates/phenotips_clinincal_labels.txt', 'r')
 #wjson = open('sample_data.txt','r').read()
 #wjdata = json.loads(wjson)
+
+with templateFile as myfile:
+	template=myfile.readlines()
+
+#print template[0]
+
+extractedData.close()
+extractedData = open('import-data/Clinical-data/PHENOTIPS_clinical.txt', 'a+')
+
 inFile = sys.argv[1]
 
 #print str(inFile)
@@ -20,15 +29,16 @@ def convertJSON(data):
 	#wjson = open(data,'r').read()	
 	wjdata = json.loads(data)
 
+	extractedData.write(template[0])
+	#extractedData.write('\n')
+
 	dataCategories = [wjdata.get("report_id"), wjdata.get("external_id"), wjdata.get('patient_name',{}).get('first_name'), 
 		wjdata.get('patient_name',{}).get('last_name'), wjdata.get("sex"), wjdata.get("life_status"), wjdata.get("date_of_birth",{}).get("year"),
 		wjdata.get("date_of_birth",{}).get("month"), wjdata.get("date_of_birth",{}).get("day"), wjdata.get("date_of_death",{}).get("year"), 
 		wjdata.get("date_of_death",{}).get("month"), wjdata.get("date_of_death",{}).get("day"), wjdata.get('ethnicity').get('maternal_ethnicity'),
 		wjdata.get('ethnicity').get('paternal_ethnicity')]
 
-	#extractedData.write('\n')
-
-	with open('import-data/Clinical-data/PHENOTIPS_clinical.txt', 'a+') as file:
+	with extractedData as file:
 		max_array_size = 1
 		file.write(wjdata.get("report_id"))
 		file.write('\t')
@@ -52,4 +62,4 @@ def convertJSON(data):
 			
 convertJSON(inFile)
 
-extractedData.close()
+#extractedData.close()
