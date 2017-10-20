0d73db7b3e4451457f7d46dc034627713b6835fc
georgezhyuan <yuangeorge9981@gmail.com>
2017-10-20 17:39:01 -0400

    created script to remove a single person from transmart

--- remove_person.py
+++ remove_person.py
@@ -1,10 +1,36 @@
-from pySQL import pySQL
+from SQL_Query import SQL_Query
+from sets import Set
 
-STUDY_ID = 'PHENO_TEST'
-SUBJECT_ID = 'P0000001'
+def find_affected_paths (direct_concept_paths):
+	found_paths = Set(direct_concept_paths)
+	new_paths = Set(found_paths)
+	while len(new_paths) != 0:	
+		discovered_paths = Set()
+		for path in new_paths:
+			parent_path = sql.query("SELECT parent_concept_path FROM i2b2demodata.concept_counts WHERE concept_path IN (SELECT c_fullname FROM i2b2metadata.i2b2 WHERE sourcesystem_cd='"+STUDY_ID+"') AND concept_path='"+path+"'")['parent_concept_path']
+			if parent_path:
+				p_path = parent_path[0]
+				if not(p_path in found_paths):
+					found_paths.add(p_path)
+					discovered_paths.add(p_path)
+		new_paths = discovered_paths
+	return found_paths
 
-sql_tbl=pySQL("SELECT patient_num FROM i2b2demodata.patient_dimension WHERE sourcesystem_cd LIKE '"+STUDY_ID+":"+SUBJECT_ID+"'")
+STUDY_ID = 'GSEXXXX'
+SUBJECT_ID = '1'
+patient_num = '0'
+sql = SQL_Query()
 
-patient_num=sql_tbl['patient_num'][0]
+patient_num = sql.query("SELECT patient_num FROM i2b2demodata.patient_dimension WHERE sourcesystem_cd LIKE '"+STUDY_ID+":"+SUBJECT_ID+"'")['patient_num'][0]
+direct_concept_paths = sql.query("SELECT a.concept_path FROM (i2b2demodata.concept_counts a INNER JOIN (SELECT b.concept_path FROM (i2b2demodata.concept_dimension b INNER JOIN (SELECT concept_cd FROM i2b2demodata.observation_fact WHERE patient_num='"+patient_num+"' AND sourcesystem_cd='"+STUDY_ID+"') c ON b.concept_cd=c.concept_cd)) d ON a.concept_path=d.concept_path)")['concept_path']
 
-print "Patient P0000001's sql patient number is: " + patient_num
+affected_paths = find_affected_paths(direct_concept_paths)
+for path in affected_paths:
+	sql.execute("UPDATE i2b2demodata.concept_counts SET patient_count=patient_count-1 WHERE concept_path='"+path+"'")
+
+sql.execute("DELETE FROM tm_lz.lt_src_clinical_data WHERE subject_id='"+SUBJECT_ID+"'")
+sql.execute("DELETE FROM i2b2demodata.patient_trial WHERE patient_num='"+patient_num+"'")
+sql.execute("DELETE FROM i2b2demodata.patient_dimension WHERE patient_num='"+patient_num+"'")
+sql.execute("DELETE FROM i2b2demodata.observation_fact WHERE patient_num='"+patient_num+"'")
+
+
