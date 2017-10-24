7562ed67d711f1cce8a7065d6b152ec1428f1a02
georgezhyuan <yuangeorge9981@gmail.com>
2017-10-24 17:06:36 -0400

    adjusted to use python scripts

--- pheno_import.sh
+++ pheno_import.sh
@@ -2,25 +2,13 @@
 
 # Initialize
 source conf/vars
-source conf/colors
-
-. ./logs/loggit.sh
-. ./get_data.sh
-. ./curl_request.sh 
 
 # Determine upload instructions
-import_method="new"
 patient_ids=('P0000001' 'P0000002' 'P0000003' 'P0000004')
 
 # Get the patient data from phenotips
-setup_clinical_file "${patient_ids[@]}"
+python src/setup_data_file.py "${patient_ids[@]}"
 
-# Create tables for upload
-if [[ $import_method == "existing" ]]; then
-	echo -e "${LCYAN}Adjusting${NC} to existing study :..."
-	#./create_tables_existing_study.sh "${patient_dataset[@]}"
-fi;
-
-# Upload to transmart
-echo -e "${LCYAN}Uploading${NC} data to transmart"
-./upload_data.sh
+## Upload to transmart
+#echo -e "${LCYAN}Uploading${NC} data to transmart"
+#./upload_data.sh
