1e02de222961b5f847308ec33f4057e6ee88d8c7
Rimpeyd <rimpeyd@gmail.com>
2017-10-24 08:10:22 -0600

    Testing extract function

--- extract_data.py
+++ extract_data.py
@@ -35,8 +35,12 @@ def convertJSON(data):
 	dataCategories = [wjdata.get("report_id"), wjdata.get("external_id"), wjdata.get('patient_name',{}).get('first_name'), 
 		wjdata.get('patient_name',{}).get('last_name'), wjdata.get("sex"), wjdata.get("life_status"), wjdata.get("date_of_birth",{}).get("year"),
 		wjdata.get("date_of_birth",{}).get("month"), wjdata.get("date_of_birth",{}).get("day"), wjdata.get("date_of_death",{}).get("year"), 
-		wjdata.get("date_of_death",{}).get("month"), wjdata.get("date_of_death",{}).get("day"), wjdata.get('ethnicity').get('maternal_ethnicity'),
-		wjdata.get('ethnicity').get('paternal_ethnicity')]
+		wjdata.get("date_of_death",{}).get("month"), wjdata.get("date_of_death",{}).get("day"), wjdata.get('ethnicity',{}).get('maternal_ethnicity'),
+		wjdata.get('ethnicity',{}).get('paternal_ethnicity')]
+
+	additionalCategories = [wjdata.get("family_history",{}).get("consanguinity"), wjdata.get("family_history",{}).get("miscarriages"), wjdata.get("family_history",{}).get("affectedRelatives")]
+
+	print additionalCategories[0]
 
 	with extractedData as file:
 		max_array_size = 1
