d36ec3583c8bc9db1c5fcfde4e142d7ed8a9307a
georgezhyuan <yuangeorge9981@gmail.com>
2017-10-24 16:59:06 -0400

    extracted parse data function from extract_data.py

---
+++ src/parse_data.py
@@ -0,0 +1,23 @@
+from global_vars import STUDY_ID, column_labels
+
+def parse_data(json):
+	extractedInformation = [STUDY_ID]
+	iteration = 0
+	arraySize = len(column_labels)
+	for current in range(0, arraySize):
+		innerArray = len(column_labels[current])
+		if (innerArray == 1):
+			extractedInformation.append(json.get(column_labels[current][0],{}))
+			iteration+=1
+		else:
+			extractedInformation.append(json.get(column_labels[current][0],{}).get(column_labels[current][1]))
+			iteration+=1
+		if ( isinstance(extractedInformation[iteration-1], list)):
+			temp = extractedInformation[iteration-1]
+			extractedInformation.remove(temp)
+			iteration-=1
+			count=len(temp)
+			for element in range(0, count):
+				extractedInformation.append(temp[element])
+				iteration+=1
+	return extractedInformation
