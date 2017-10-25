a90eee9076003438ade3c224eba0a9ba44b4dec2
georgezhyuan <yuangeorge9981@gmail.com>
2017-10-25 13:54:53 -0400

    setting changes

--- conf/Config.groovy
+++ conf/Config.groovy
@@ -3,9 +3,7 @@ db.port = 5432
 db.sid = 'xe'
 db.username = 'transmart'
 db.password = 'transmart'
-
 db.jdbcConnectionString = "jdbc:postgresql://${db.hostname}:${db.port}/transmart"
 db.jdbcDriver = 'org.postgresql.Driver'
 db.sql.storedProcedureSyntax = 'PostgreSQL'
-
 dataDir = "/home/transmart/Pheno-Import/import-data"
