cdffc42206f993593b519a1c5acf64fb0a8c568b
georgezhyuan <yuangeorge9981@gmail.com>
2017-10-25 13:08:17 -0400

    moved parseJSON into Data_File class as add patient

--- src/Data_File.py
+++ src/Data_File.py
@@ -1,33 +1,37 @@
+from Structs import Transmart_Study
+
 class Data_File:
-	def __init__(self, label_file="templates/data_labels.txt"):
-		self.label_location = label_file
-		self.__set_labels()
-		self.reset()
-
-
-	def reset (self):
+	def __init__(self, tm, hm_dir):		
+		self.study_id = tm.STUDY_ID
+		self.data_file_location = hm_dir+'/import-data'+'/'+tm.TOP_NODE+'/'+tm.STUDY_NAME+'/ClinicalData/PHENOTIPS_clinical.txt'
 		self.patient_info_list = []
-		self.data_location = ""
-		
+		self.data_labels = open(hm_dir+'/templates/data_labels.txt').readline()
+	
 
-	def __set_labels(self):
-		self.labels = open(self.label_location).readline()
-
-
-	def set_location(self, hm_dir, top_node, study_name):
-		self.data_location = hm_dir+'/import-data'+'/'+top_node+'/'+study_name+'/ClinicalData/PHENOTIPS_clinical.txt'
-
-
-	def add_patient(self, patient_data):
-		self.patient_info_list.append(patient_data)
+	def add_patient(self, json, column_labels):
+		extractedInformation = [self.study_id]
+		for label in column_labels:
+			if (len(label) == 1):
+				value = json.get(label[0],{})
+				if value == {}:
+					value = ''
+				extractedInformation.append(value)
+			else:
+				temp = json.get(label[0],{})
+				for i in range(1, len(label)):
+					temp = temp.get(label[i],{})
+				if temp == {}:
+					temp = ''
+				extractedInformation.append(temp)
+		self.patient_info_list.append(extractedInformation)
 
 
 	def generate_file(self):
-		file = open(self.data_location, 'w+')
-		file.write(self.labels)
+		file = open(self.data_file_location, 'w+')
+		file.write(self.data_labels)
 		file.close
 		
-		with open(self.data_location, 'a+') as file:
+		with open(self.data_file_location, 'a+') as file:
 			for patient_info in self.patient_info_list:
 				max_array_size = 1
 				for item in patient_info:
--- src/setup_data_file.py
+++ src/setup_data_file.py
@@ -2,22 +2,19 @@ import sys
 from Data_File import Data_File
 from global_vars import TM, PH, PH_HOME
 from phenotips_REST import phenotips_REST
-from parse_data import parse_data
 
 patient_ids = sys.argv[1:]
 patients_found = []
 patients_ommited = []
 
-data_file = Data_File(PH_HOME+'/templates/data_labels.txt')
-data_file.set_location(PH_HOME, TM.TOP_NODE, TM.STUDY_NAME)
+data_file = Data_File(TM, PH_HOME)
 
 for patient_id in patient_ids:
 	response = phenotips_REST('patients/'+patient_id)
 	if isinstance(response, int):
 		patients_ommited.append(patient_id)
 	else:
-		data = [TM.STUDY_ID]+parse_data(response, PH.JSON_LABELS)
-		data_file.add_patient(data)
+		data_file.add_patient(response, PH.JSON_LABELS)
 		patients_found.append(patient_id)
 
 data_file.generate_file()
