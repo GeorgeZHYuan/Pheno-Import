5ba2a0dceb8a2a8c0c2ee152fbbd4f8b03791db4
georgezhyuan <yuangeorge9981@gmail.com>
2017-10-25 20:05:47 -0400

    removed old scripts folder

--- old/create_clinical_file.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-phenotips_labels = open(sys.argv[4]).readline()
-phenotips_labels = phenotips_labels[7:]
-existing_labels = open('tmp/existing_labels.tmp').readline()
-column_labels = existing_labels+phenotips_labels
-
-with open('tmp/clinical_file_labels.tmp', 'w') as clin_file:
-	clin_file.write(column_labels)
