b0a5edad85f449746935ee7c41083fd91b7fb8b1
georgezhyuan <yuangeorge9981@gmail.com>
2017-11-08 15:39:25 -0500

    slight change in code layout

---
+++ initialize.sh
@@ -0,0 +1,37 @@
+# Analysis Job Variable
+source $HOME/.Pheno_Settings.config
+
+# Upload variables
+variable=("$@")
+TM_TOP_NODE=${variable[0]}
+TM_STUDY_NAME=${variable[1]}
+TM_STUDY_ID=${variable[2]}
+PHENO_ADDRESS=${variable[3]}
+PHENO_USER=${variable[4]}
+PHENO_PWD=${variable[5]}
+
+echo $TM_TOP_NODE
+echo $PHENO_USER
+echo $PHENO_PWD
+
+# Convert to python variables
+echo "# Setup python global variable
+import sys
+from Data_Types import *
+
+# Pheno-Import Paths
+PH_HOME='$PH_HOME'
+
+# Transmart Variables
+TM = Transmart_Study()
+TM.TOP_NODE = '$TM_TOP_NODE'
+TM.STUDY_NAME = '$TM_STUDY_NAME'
+TM.STUDY_ID = '$TM_STUDY_ID'
+
+# Phenotips Variables
+PH = Pheno_Settings()
+PH.ADDRESS = '$PHENO_ADDRESS'
+PH.USER = '$PHENO_USER'
+PH.PWD = '$PHENO_PWD'" > $PH_HOME/src/upload_vars.py
+
+
--- src/phenotips_REST.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def phenotips_REST(phenotips, args):
-	base_url = 'http://'+phenotips.ADDRESS+'/rest/'
-	r = requests.get(base_url + args, auth=(phenotips.USER, phenotips.PWD))
-	if r.status_code != 200:
-		return r.status_code
-	else:
-		return r.json()
