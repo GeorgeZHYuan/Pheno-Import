b37da7d30d93e8fcfc9951fd6385fdeef01a6369
georgezhyuan <yuangeorge9981@gmail.com>
2017-11-08 15:51:26 -0500

    reformatting

--- pheno_import.sh
+++ pheno_import.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-# Initialize variables
 source $HOME/.Pheno_Settings.config
 
-# TOP NODE \ Study Name \ Study ID \ Phenotips Address \ Phenotips Username \ Phenotips Password
-#upload_vars=("{$@:1:6}") 
-upload_vars=('Public Studies' 'Phenotips' 'PHENOTEST' 'localhost:10000' 'Admin' 'admin')
-
-$PH_HOME/initialize.sh "${upload_vars[@]}"
-
-# Determine patients to upload
+## Variables from bash
+#upload_vars=("{$@:1:6}")
 #patient_ids=("${@:7}")
+
+# Manually Input Variables
+upload_vars=('Public Studies' 'Phenotips' 'PHENOTEST' 'localhost:10000' 'Admin' 'admin')
 patient_ids=('P0000001' 'P0000002' 'P0000003' 'P0000004' 'P0000005' 'P0000006')
 
 # Get the patient data from phenotips
+$PH_HOME/initialize.sh "${upload_vars[@]}"
 python $PH_HOME/src/setup_data_file.py "${patient_ids[@]}"
 
 # Upload to transmart
