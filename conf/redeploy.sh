31d5791be246782cdf860e8af4062dfaa87a837f
georgezhyuan <yuangeorge9981@gmail.com>
2017-12-04 16:47:42 -0700

    fixed bugs

--- conf/redeploy.sh
+++ conf/redeploy.sh
@@ -1,20 +1,19 @@
 # Location Variables
 compile_location=$HOME/Pheno-Import/conf/AnalysisJobsImport
 edit_location=/var/lib/tomcat7/webapps
-
+rmod_ctrl_location=/classes/com/recomdata/transmart/data/association
+web_apps_location=/transmart/plugins/rdc-rmodules-16.2
 
 # Adding new files
-if [ ! -d $edit_location/transmart/plugins/rdc-rmodules-16.2/Rscripts/DataUpload ]; then
-	mkdir $edit_location/transmart/plugins/rdc-rmodules-16.2/Rscripts/DataUpload
-fi
+sudo mkdir $edit_location$web_apps_location/Rscripts/DataUpload
 
 sudo cp $compile_location/DataUploadController/DataUploadController*.class $edit_location/transmart/WEB-INF$rmod_ctrl_location
 sudo cp $compile_location/DataUpload/DataUpload*.class $edit_location/transmart/WEB-INF/classes/jobs
 
-sudo cp $compile_location/Rscripts/DataUpload.R $edit_location/transmart/plugins/rdc-rmodules-16.2/Rscripts/DataUpload
+sudo cp $compile_location/Rscripts/DataUpload.R $edit_location$web_apps_location/Rscripts/DataUpload
 sudo cp $compile_location/views/DataUpload.gsp $edit_location/transmart/WEB-INF/plugins/rdc-rmodules-16.2/grails-app/views/plugin
 sudo cp $compile_location/views/_dataUpload_out.gsp $edit_location/transmart/WEB-INF/plugins/rdc-rmodules-16.2/grails-app/views/plugin
-sudo cp $compile_location/js/DataUpload.js $edit_location/transmart/plugins/rdc-rmodules-16.2/js/plugin
+sudo cp $compile_location/js/DataUpload.js $edit_location$web_apps_location/js/plugin
 sudo cp $compile_location/phenoimport.war $edit_location
 
 
@@ -31,17 +30,18 @@ sudo cp $compile_location/phenoimport.war $edit_location
 # *********                                                   *********
 # *********************************************************************
 # *********************************************************************
-
 # Directly inserting editted files
 sudo cp $compile_location/RModulesController/RModulesController*.class $edit_location/transmart/WEB-INF$rmod_ctrl_location
 sudo cp $compile_location/RmodulesResources/RmodulesResources*.class $edit_location/transmart/WEB-INF/classes
 sudo cp $compile_location/grails.xml $edit_location/transmart/WEB-INF
-sudo cp $compile_location/css/rmodules.css $edit_location/transmart/plugins/rdc-rmodules-16.2/css
+sudo cp $compile_location/css/rmodules.css $edit_location$web_apps_location/css
 
 
 # Restart tomcat7 to apply changes
 sudo service tomcat7 restart
 
 
-
+#register analysis job page
+command="insert into searchapp.plugin_module(module_seq, plugin_seq, name, module_name, form_page, params) values (112 + 1, 1, 'Upload Phenotips Data', 'dataUpload', 'DataUpload', '{}');"
+echo "$command" |  sudo -u postgres psql transmart
 
