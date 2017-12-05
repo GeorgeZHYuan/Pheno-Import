a83edd0c981bcffc3f96e88c92e81ca5d105d7e6
georgezhyuan <yuangeorge9981@gmail.com>
2017-12-05 10:22:40 -0700

    adjusted config procedure

--- config.sh
+++ config.sh
@@ -5,9 +5,6 @@ sudo apt-get update
 sudo apt-get install python-pip
 sudo pip install requests
 
-ADDRESS=$TRANSMART_DB_HOST:$TRANSMART_DB_PORT
-echo "$ADDRESS:transmart:$TRANSMART_DB_USR:$TRANSMART_DB_PWD" > ~/.pgpass
-
 # Local installation of tMDataLoader
 if [[ $TM_DATALOADER_PATH == 'install local version' ]]; then
 	TM_DATALOADER_PATH=$PH_HOME/tMDataLoader
@@ -25,24 +22,27 @@ if [[ $TM_DATALOADER_PATH == 'install local version' ]]; then
 		chmod 0600 ~/.pgpass
 	fi;
 
+	ADDRESS=$TRANSMART_DB_HOST:$TRANSMART_DB_PORT
+	echo "$ADDRESS:transmart:$TRANSMART_DB_USR:$TRANSMART_DB_PWD" > ~/.pgpass
+
 	# setup sql scripts for tMDataLoader
 	cd sql/postgres
 	psql -d transmart -U postgres -f migrations.sql -h $TRANSMART_DB_HOST
 	psql -d transmart -U postgres -f permissions.sql -h $TRANSMART_DB_HOST
 	psql -d transmart -U $TRANSMART_DB_USR -f procedures.sql -h $TRANSMART_DB_HOST
-fi;
 
-# Setup tMDataLoader config file
-TM_CONFIG_FILE_PATH=$PH_HOME/conf/Config.groovy
-echo "dataDir = '$PH_HOME/import-data'
-db.hostname = '$TRANSMART_DB_HOST'
-db.port = '$TRANSMART_DB_PORT'
-db.username = '$TRANSMART_DB_USR'
-db.password = '$TRANSMART_DB_PWD'
-db.jdbcConnectionString = 'jdbc:postgresql://$ADDRESS/transmart'
-db.jdbcDriver = 'org.postgresql.Driver'
-db.sql.storedProcedureSyntax = 'PostgreSQL'
-db.sid = 'xe'" > $TM_CONFIG_FILE_PATH
+	# Setup tMDataLoader config file
+	TM_CONFIG_FILE_PATH=$PH_HOME/conf/Config.groovy
+	echo "dataDir = '$PH_HOME/import-data'
+	db.hostname = '$TRANSMART_DB_HOST'
+	db.port = '$TRANSMART_DB_PORT'
+	db.username = '$TRANSMART_DB_USR'
+	db.password = '$TRANSMART_DB_PWD'
+	db.jdbcConnectionString = 'jdbc:postgresql://$ADDRESS/transmart'
+	db.jdbcDriver = 'org.postgresql.Driver'
+	db.sql.storedProcedureSyntax = 'PostgreSQL'
+	db.sid = 'xe'" > $TM_CONFIG_FILE_PATH
+fi;
 
 # Setup Pheno-Import settings
 echo "PH_HOME=$PH_HOME 
