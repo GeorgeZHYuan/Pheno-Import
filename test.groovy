484962a6af5007c428f6509700dcf378b11e8abb
George Yuan <yuangeorge9981@gmail.com>
2017-12-05 11:40:21 -0700

    Delete test.groovy

--- test.groovy
+++
@@ -1,5 +0,0 @@
-def sout = new StringBuilder(), serr = new StringBuilder()
-def proc = './pheno-import.sh'.execute()
-proc.consumeProcessOutput(sout, serr)
-proc.waitForOrKill(5000)
-println "out> $sout. err> $serr"
